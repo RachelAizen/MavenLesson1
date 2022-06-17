9b4dbf21266cc067c4e4220bca6d98446dc6137f
888 <246rcl@gmail.com>
2022-06-17 12:00:34 +0300

    Added a print.

--- src/main/java/MavenLesson7.java
+++ src/main/java/MavenLesson7.java
@@ -2,7 +2,8 @@ import org.testng.annotations.Test;
 
 public class MavenLesson7 {
     @Test
-    public void Maventesting(){}
-
+    public void Maventesting(){
+        System.out.println("My first Maven Lesson!");
+    }
 }
 
